c830ac091d86977ead16fd62f8961d0c45e6e5b3
poenie <andre.oli4@telkomsa.net>
2015-04-25 08:17:31 +0200

    Programming Assigment 2 Caching Inverted Matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,66 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The set of functions below have the primary purpose of providing an inverted matrix. 
+## Secondary to that, it provides a neat caching mechanism to improve performance, since inverting  
+## a matrix could be costly in terms of resource usage. Therefore, these functions will save time and 
+## memory usage.
+## The idea is that the "makeCacheMatrix" function is called first, to set up the cache (initially not inverted)
+##, and then "cacheSolve" to invert the matrix.
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a list containing a function to
+## 1. set the value of the matrix
+## 2. get the value of the matrix
+## 3. set the value of inverse of the matrix
+## 4. get the value of inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse <- function() inv
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function returns an inverted matrix 
+## It also contains logic to work out whether the matrix has been cached or not
+## If it detects a cached matrix, it will return the cached data
+## If no cache can be found, it will run the inversion logic
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
+    message("getting cached data.")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data)
+  x$setinverse(inv)
+  inv
 }
+
+## Example usage:
+## > x = rbind(c(1, -1/4), c(-1/4, 1))
+## > m = makeCacheMatrix(x)
+## > m$get()
+##       [,1]  [,2]
+## [1,]  1.00 -0.25
+## [2,] -0.25  1.00
+
+## No cache in the first run
+## > cacheSolve(m)
+##           [,1]      [,2]
+## [1,] 1.0666667 0.2666667
+## [2,] 0.2666667 1.0666667
+
+## Retrieving from the cache in the second run
+## > cacheSolve(m)
+## getting cached data.
+##           [,1]      [,2]
+## [1,] 1.0666667 0.2666667
+## [2,] 0.2666667 1.0666667
+## > 
